3e3f5de2d62c04a6328c5dc92a6517e91e45fee0
kajal <tiwarikajal1306@gmail.com>
2020-08-17 11:03:09 +0530

    ADDED Calculated employee wage for part time

--- employeeWageComputationProblem.cpp
+++ employeeWageComputationProblem.cpp
@@ -5,18 +5,24 @@
 using namespace std;
 
 const int EMP_RATE_PER_HOUR = 20;
+const int IS_PART_TIME = 1;
+const int IS_FULL_TIME = 2;
 
 int empHrs = 0;
 int empWage = 0;
 
 int main() {
 	srand(time(0));
-	int is_Present = rand() % 2 + 1;
-	if(is_Present == 1){
+	int employee_Check = rand() % 3 + 1;
+	if( employee_Check == IS_PART_TIME ) {
+		empHrs = 4;
+		cout << "Employee wage for Part Time" << endl;
+	} else if( employee_Check == IS_FULL_TIME ) {
 		empHrs = 8;
-	}
-	else{
+		cout << "Employee wage for Full Time" << endl;
+	} else {
 		empHrs = 0;
+		cout << "Employee is absent" << endl;
 	}
 
 	empWage = empHrs * EMP_RATE_PER_HOUR;
