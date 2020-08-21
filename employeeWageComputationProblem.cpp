f645b8973ca29ee11d0f2d1cf81dace5c6fbb769
kajal <tiwarikajal1306@gmail.com>
2020-08-21 22:46:18 +0530

    ADDED Calculated EmpWage for multiple company store in array

--- employeeWageComputationProblem.cpp
+++ employeeWageComputationProblem.cpp
@@ -8,79 +8,74 @@ using namespace std;
 
 struct EmpWageBuilder
 {
+	string empName;
+	int months;
+	string companyName;
+	int  EMP_RATE_PER_HOUR;
+	int NUMBER_OF_WORKING_DAYS;
+	int MAX_HRS_IN_MONTH;
 
-string empName;
-int months;
-string companyName;
-int  EMP_RATE_PER_HOUR;
-int NUMBER_OF_WORKING_DAYS;
-int MAX_HRS_IN_MONTH;
+	void employeeDetails( string name, int months, string companyName, int  EMP_RATE_PER_HOUR, int NUMBER_OF_WORKING_DAYS , int MAX_HRS_IN_MONTH ) {
+		this -> empName = name;
+		this -> months = months;
+		this -> companyName = companyName;
+		this ->  EMP_RATE_PER_HOUR =  EMP_RATE_PER_HOUR;
+		this -> NUMBER_OF_WORKING_DAYS = NUMBER_OF_WORKING_DAYS;
+		this -> MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+	}
+};
 
-EmpWageBuilder( string name, int months, string companyName, int  EMP_RATE_PER_HOUR, int NUMBER_OF_WORKING_DAYS , int MAX_HRS_IN_MONTH ) {
-	this -> empName = name;
-	this -> months = months;
-	this -> companyName = companyName;
-	this ->  EMP_RATE_PER_HOUR =  EMP_RATE_PER_HOUR;
-	this -> NUMBER_OF_WORKING_DAYS = NUMBER_OF_WORKING_DAYS;
-	this -> MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
-}
-
-
-int empHrs = 0;
-int totalEmpHrs = 0;
-int empWage = 0;
-int totalEmpWage = 0;
-int totalWorkingDays = 0;
-
-
-void employeeWage()
+struct EmpWage
 {
-	const int IS_PART_TIME = 1;
-	const int IS_FULL_TIME = 2;
 
-	srand(time(0));
+	void employeeWage(EmpWageBuilder empWageBuilder)
+	{
 
-	fstream fileStream;
-	fileStream.open( "EmployeeWage.csv", ios::out | ios::app );
-	fileStream << "Day" << "," << "CompanyName" << "," << "Name" << "," << "TotalEmpHrs" << "," << "TotalEmpWage" << endl;
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
 
-	while ( totalEmpHrs < MAX_HRS_IN_MONTH * months && totalWorkingDays < NUMBER_OF_WORKING_DAYS * months  ) {
-		totalWorkingDays++;
-		int employee_Check = rand() % 3 + 1;
+		const int IS_PART_TIME = 1;
+		const int IS_FULL_TIME = 2;
 
-		switch( employee_Check ) {
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
+		srand(time(0));
 
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			default:
-				empHrs = 0;
+		fstream fileStream;
+		fileStream.open( "EmployeeWage.csv", ios::out | ios::app );
+		fileStream << "Day" << "," << "CompanyName" << "," << "Name" << "," << "TotalEmpHrs" << "," << "TotalEmpWage" << endl;
+
+		while( totalEmpHrs < empWageBuilder.MAX_HRS_IN_MONTH * empWageBuilder.months &&
+			 totalWorkingDays < empWageBuilder.NUMBER_OF_WORKING_DAYS * empWageBuilder.months ) {
+
+			totalWorkingDays++;
+			int employee_Check = rand() % 3 + 1;
+
+			switch( employee_Check ) {
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
+			}
+
+			totalEmpHrs +=empHrs;
+			totalEmpWage = totalEmpHrs * empWageBuilder.EMP_RATE_PER_HOUR;
+			fileStream << totalWorkingDays << "," << empWageBuilder.companyName << "," << empWageBuilder.empName << "," << totalEmpHrs << "," << totalEmpWage << endl;
 		}
 
-		totalEmpHrs +=empHrs;
-		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		fileStream << totalWorkingDays << "," << companyName << "," << empName << "," << totalEmpHrs << "," << totalEmpWage << endl;
+		fileStream.close();
+
+		totalEmpWage = totalEmpHrs * empWageBuilder.EMP_RATE_PER_HOUR;
+		cout << "employee wage is : "<< totalEmpWage << endl;
+
+
 	}
-	fileStream.close();
-
-	totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-	cout << "employee wage is : "<< totalEmpWage << endl;
-
-	fstream fin;
-	string data;
-	cout<<"\nCSV file has been successfully created..!!";
-        cout<<"\n\nDisplaying the content of CSV file=>\n\n";
-
-	fin.open( "EmployeeWage.csv", ios::in );
-
-		 while( getline (fin, data) ) {
-                        cout << data << endl;
-                }
-
-}
 };
 
 int main()
@@ -89,15 +84,16 @@ int main()
         fstream fileStream;
         fileStream.open( "EmployeeWage.csv", ios::out | ios::trunc );
 
-	int totalMonths;
+	int months;
 	cout << "Enter the number of month month" << endl;
-	cin >> totalMonths;
+	cin >> months;
 
-	EmpWageBuilder bridgeLabz("Kajal", totalMonths, "BridgeLabz", 20, 20, 100);
-	bridgeLabz.employeeWage();
-	sleep(3);
+	struct EmpWageBuilder empWageBuilder[2];
+	empWageBuilder[0].employeeDetails("Sonali", months, "BridgeLabz", 20, 10, 60);
+	empWageBuilder[1].employeeDetails("NiKita", months, "Amazon", 20, 25, 70);
 
-	EmpWageBuilder amazon("NiKita", totalMonths, "Amazon", 20, 25, 70);
-	amazon.employeeWage();
+	struct EmpWage empWage;
+	empWage.employeeWage(empWageBuilder[0]);
+	empWage.employeeWage(empWageBuilder[1]);
 	return 0;
 }
